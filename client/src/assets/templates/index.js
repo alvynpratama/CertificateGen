af0b97654c64e6d9db3541f92aa8743421d26a33
Kifansa <kifansafadhlurrohman5@gmail.com>
2026-01-09 19:37:33 +0700

    update template

--- client/src/assets/templates/index.js
+++ client/src/assets/templates/index.js
@@ -4,13 +4,11 @@
 import t1 from './Template1.png';
 import t2 from './Template2.png';
 import t3 from './Template3.png';
-import t4 from './Template4.jpeg'; // Perhatikan .jpeg
+import t4 from './Template4.png'; 
 import t5 from './Template5.png';
 import t6 from './Template6.png';
 import t7 from './Template7.png';
 import t8 from './Template8.png';
-import t9 from './Template9.jpg';  // Perhatikan .jpg
-import t10 from './Template10.png'; 
 
 // Jika Template11 belum ada gambarnya, JANGAN dipanggil disini dulu agar tidak error.
 // import t11 from './Template11.png'; 
@@ -24,8 +22,6 @@ const templates = {
     template6: t6,
     template7: t7,
     template8: t8,
-    template9: t9,
-    template10: t10,
     // template11: t11, // Saya komen dulu agar tidak error
 };
 
